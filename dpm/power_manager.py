d59c78f26a19784245fa430ca13f334a41dfb61c
Lorenzo Marino <lorenzo.marino1997@gmail.com>
2024-09-04 17:55:50 +0200

    enhanced the power manager controller python generator - it is now able to extract automatically the componend and create the methods for each voltage and power port - stiil need to generate cpp code accordingly

--- dpm/power_manager.py
+++ dpm/power_manager.py
@@ -1,37 +1,31 @@
 import gvsoc.systree as gsys 
 
+def add_ports(component_list):
+    #scans the component list and adds power and voltage port on the class
+    for component in component_list:
+            power_port_name = "o_POWER_CTRL_"+ component
+            voltage_port_name = "o_VOLTAGE_CTRL_"+ component
+            def power_ports(self, itf: gsys.SlaveItf, name=f'power_ctrl_{component}'):
+                self.itf_bind(name, itf, signature='wire<int>')
+
+            def voltage_ports(self, itf: gsys.SlaveItf, name=f'voltage_ctrl_{component}'):
+                self.itf_bind(name, itf, signature='wire<int>')
+            setattr(PowerManager, power_port_name, power_ports)
+            setattr(PowerManager, voltage_port_name, voltage_ports)
+    
+    #port needs to be added also in the cpp file.
+
+
+
 class PowerManager(gsys.Component):
     def __init__(self, parent: gsys.Component, name: str):
-
         super().__init__(parent, name)
         self.add_sources(["power_manager.cpp"])
         self.add_properties
-
-
+        self.component_list = parent.components
+        print("detected components: ", self.component_list )
+        add_ports(self.component_list)
+        
     def i_INPUT(self) -> gsys.SlaveItf:
         return gsys.SlaveItf(self, 'input', signature='io')
-    
-    def o_POWER_CTRL(self, itf: gsys.SlaveItf):
-        self.itf_bind('power_ctrl', itf, signature='wire<int>')
 
-    def o_VOLTAGE_CTRL(self, itf: gsys.SlaveItf):
-        self.itf_bind('voltage_ctrl', itf, signature='wire<int>')
-
-
-    def o_POWER_CTRL_1(self, itf: gsys.SlaveItf):
-        self.itf_bind('power_ctrl1', itf, signature='wire<int>')
-
-    def o_VOLTAGE_CTRL_1(self, itf: gsys.SlaveItf):
-        self.itf_bind('voltage_ctrl1', itf, signature='wire<int>')
-
-    def o_POWER_CTRL_2(self, itf: gsys.SlaveItf):
-        self.itf_bind('power_ctrl2', itf, signature='wire<int>')
-
-    def o_VOLTAGE_CTRL_2(self, itf: gsys.SlaveItf):
-        self.itf_bind('voltage_ctrl2', itf, signature='wire<int>')
-
-    def o_POWER_CTRL_3(self, itf: gsys.SlaveItf):
-        self.itf_bind('power_ctrl3', itf, signature='wire<int>')
-
-    def o_VOLTAGE_CTRL_3(self, itf: gsys.SlaveItf):
-        self.itf_bind('voltage_ctrl3', itf, signature='wire<int>')
