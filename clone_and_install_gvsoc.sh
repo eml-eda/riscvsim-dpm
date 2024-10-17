b8dd19e7fd87c183c4d5b70e4cc26c2831834f9e
Lorenzo Marino <lorenzo.marino1997@gmail.com>
2024-10-17 17:44:31 +0200

    uploaded script to install gvsoc (eml-eda  version)

--- clone_and_install_gvsoc.sh
+++ clone_and_install_gvsoc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #clone the gvsoc repository
-git clone https://github.com/gvsoc/gvsoc.git
+git clone https://github.com/eml-eda/gvsoc.git
 
 #install prerequirements
 sudo apt-get install -y build-essential git doxygen python3-pip libsdl2-dev curl cmake gtkwave libsndfile1-dev rsync autoconf automake texinfo libtool pkg-config libsdl2-ttf-dev
 
 #move to gvsoc home directory, install dependencies and simulator
 cd gvsoc
-git submodule update --init --recursive --jobs 8
+git submodule update --init --recursive --jobs 4
 pip3 install -r core/requirements.txt
 pip3 install -r gapy/requirements.txt
 make all
